342855599db8a53cba43de3b9ea1ce137a3a712a
HBFLEX <happybanda@dyuni.ac.mw>
2024-06-09 15:03:56 +0200

    remove isar db

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:note_plus/screens/home/home_screen.dart';
 
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
-  // ignore: unused_local_variable
-  await Hive.openBox('todosDB');
   runApp(const MainApp());
 }
 
@@ -25,7 +21,7 @@ class MainApp extends StatelessWidget {
           ),
         ),
       ),
-      home: HomeScreen(),
+      home: const HomeScreen(),
     );
   }
 }
